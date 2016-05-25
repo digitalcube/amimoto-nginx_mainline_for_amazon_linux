ddcc7c6c2998d06f6a6811281609954ee6f34f59
sawanoboly <sawanoboriyu@higanworks.com>
2016-05-25 18:09:37 +0900

    install tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 set -ex
 
 sudo yum -y update
-sudo yum -y install rpm-build git gcc make autoconf
+sudo yum -y install rpm-build git gcc make autoconf tmux
 sudo yum -y install pcre-devel openssl-devel libxml2-devel libxslt-devel gd-devel perl-devel perl-ExtUtils-Embed geoip-devel gperftools-devel
 
