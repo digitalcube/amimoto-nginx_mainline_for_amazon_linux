9119aaec74b2e763e2541a126634419e7edd170f
sawanoboly <sawanoboriyu@higanworks.com>
2016-06-04 17:14:34 +0900

    change rpmbuild target dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,7 @@ sudo yum -y install pcre-devel openssl-devel libxml2-devel libxslt-devel \
   gd-devel perl-devel perl-ExtUtils-Embed geoip-devel gperftools-devel \
   rubygem-rake bison
 
-if [ -d ~/rpmbuild ]; then
-  cd ~/rpmbuild && git fetch origin
-else
-  git clone https://github.com/OpsRockin/nginx_preview_for_amimoto.git ~/rpmbuild
+if [ ! -d /home/ec2-user/rpmbuild ]; then
+  git clone https://github.com/OpsRockin/nginx_preview_for_amimoto.git /home/ec2-user/rpmbuild
+  chown -R ec2-user.ec2-user /home/ec2-user/rpmbuild
 fi
