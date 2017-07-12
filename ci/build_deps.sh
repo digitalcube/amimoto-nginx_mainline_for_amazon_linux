76d83553ddd84a57649a5942535020dcba519041
sawanoboly <sawanoboriyu@higanworks.com>
2017-07-12 15:15:04 +0900

    install deps by shell

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -7,5 +7,7 @@ yum -y update && yum -y install rpm-build git gcc make autoconf tmux \
   gd-devel perl-devel perl-ExtUtils-Embed geoip-devel gperftools-devel \
   rubygem-rake bison wget
 
-wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
-wget https://github.com/OpsRockin/ngx_cache_purge/archive/2.3.dynamic.tar.gz -O SOURCES/ngx_cache_purge_2.3.dynamic.tar.gz
+if [ "$CIRCLE_JOB" == "build" ] ; then
+  wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
+  wget https://github.com/OpsRockin/ngx_cache_purge/archive/2.3.dynamic.tar.gz -O SOURCES/ngx_cache_purge_2.3.dynamic.tar.gz
+fi
