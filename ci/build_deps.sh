b5f81e527d84d9ca74917f7921598776e2217f31
sawanoboly <sawanoboriyu@higanworks.com>
2019-10-17 20:31:15 +0900

    AMZN_VERSION

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -10,20 +10,20 @@ yum -y update && yum -y install rpm-build git gcc make autoconf tmux \
   gd-devel perl-devel perl-ExtUtils-Embed geoip-devel gperftools-devel \
   rubygem-rake bison wget gcc-c++ libuuid-devel
 
-if [ "$AMZN_VERSIONN" == "1" ] ; then
+if [ "$AMZN_VERSION" == "1" ] ; then
   wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
   wget https://github.com/OpsRockin/ngx_cache_purge/archive/2.3.dynamic.tar.gz -O SOURCES/ngx_cache_purge_2.3.dynamic.tar.gz
   wget https://codeload.github.com/pagespeed/ngx_pagespeed/tar.gz/v${NPS_VERSION}-stable -O SOURCES/incubator-pagespeed-ngx_${NPS_VERSION}.tar.gz
   wget https://dl.google.com/dl/page-speed/psol/${PSOL_VERSION}-x64.tar.gz -O SOURCES/psol_${PSOL_VERSION}.tar.gz
 fi
 
-if [ "$AMZN_VERSIONN" == "2" ] ; then
+if [ "$AMZN_VERSION" == "2" ] ; then
   wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
   wget https://github.com/OpsRockin/ngx_cache_purge/archive/2.3.dynamic.tar.gz -O SOURCES/ngx_cache_purge_2.3.dynamic.tar.gz
   wget https://codeload.github.com/pagespeed/ngx_pagespeed/tar.gz/v${NPS_VERSION}-stable -O SOURCES/incubator-pagespeed-ngx_${NPS_VERSION}.tar.gz
   wget https://dl.google.com/dl/page-speed/psol/${PSOL_VERSION}-x64.tar.gz -O SOURCES/psol_${PSOL_VERSION}.tar.gz
 fi
 
-if [ "$AMZN_VERSIONN" == "2" ] ; then
+if [ "$AMZN_VERSION" == "2" ] ; then
   yum -y install openssl initscripts
 fi
