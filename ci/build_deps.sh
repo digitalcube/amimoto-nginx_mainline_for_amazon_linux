3187733ec7590a3baf89faf6271585a58731d786
sawanoboly <sawanoboriyu@higanworks.com>
2019-12-25 15:03:20 +0900

    download openssl targz

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -12,6 +12,7 @@ yum -y update && yum -y install rpm-build git gcc make autoconf tmux \
 
 if [ "$CIRCLE_JOB" == "build1" ] ; then
   yum -y install openssl-devel
+  wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}-latest.tar.gz -O SOURCES/openssl-${OPENSSL_VERSION}-latest.tar.gz
   wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
   wget https://github.com/OpsRockin/ngx_cache_purge/archive/2.3.dynamic.tar.gz -O SOURCES/ngx_cache_purge_2.3.dynamic.tar.gz
   wget https://codeload.github.com/pagespeed/ngx_pagespeed/tar.gz/v${NPS_VERSION}-stable -O SOURCES/incubator-pagespeed-ngx_${NPS_VERSION}.tar.gz
