381a3529fcf01087f93f88a33c6c4bdbf6957b4c
sawanoboly <sawanoboriyu@higanworks.com>
2023-10-12 12:27:15 +0900

    use https and update nginx_version

--- ci/build_deps.sh
+++ ci/build_deps.sh
@@ -13,14 +13,14 @@ yum -y update && yum -y install rpm-build git gcc make autoconf tmux \
 if [ "$CIRCLE_JOB" == "build1" ] ; then
   source ./OPENSSL_VERSION
   wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}${OPENSSL_SUBVERSION}.tar.gz -O SOURCES/openssl-${OPENSSL_VERSION}-latest.tar.gz
-  wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
+  wget https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
   wget https://github.com/OpsRockin/ngx_cache_purge/archive/2.3.dynamic.tar.gz -O SOURCES/ngx_cache_purge_2.3.dynamic.tar.gz
 fi
 
 if [ "$CIRCLE_JOB" == "build2" ] ; then
   source ./OPENSSL_VERSION
   wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}${OPENSSL_SUBVERSION}.tar.gz -O SOURCES/openssl-${OPENSSL_VERSION}-latest.tar.gz
-  wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
+  wget https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O SOURCES/nginx-${NGINX_VERSION}.tar.gz
   wget https://github.com/OpsRockin/ngx_cache_purge/archive/2.3.dynamic.tar.gz -O SOURCES/ngx_cache_purge_2.3.dynamic.tar.gz
 fi
 
